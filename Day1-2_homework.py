0e3b512f45765b646a8b77ecffd8b26182ad24cb
Hitler <tomodemai@outlook.com>
2020-08-21 12:05:59 +0800

    Day1-2 hoomework

--- Day1-2_homework.py
+++ Day1-2_homework.py
@@ -1,6 +1,133 @@
-# 第四章Python的数据类型之字符串和整型
-str
+# 第二章Python基础入门
+# 1.	gbk，utf-8，ascii，unicode
+#
+# 2.	1.>print变为内置函数需要加括号
+#     2.>isinstance判断变化
+#     3.>增加了新式类
+#     4.>range和xrange合并为range（生成器）
+#     5.>编码统一使用utf-8
+#
+# 3.	1.str，int，
 
+value = """51devops"niubi"""
+
+print("""
+    ⽂能提笔安天下,
+    武能上⻢定乾坤.
+    ⼼存谋略何⼈胜,
+    古今英雄唯是君。
+    """)
+
+# 数字，字母下滑下划线组合而成，不能以数字开头，不要以关键字命名。命名要有意义，尽量使用驼峰命名法或者小驼峰还有下划线命名法
+#
+# name = '51devops'
+# _ = 'echo'
+
+# if 条件语句：
+#         满足条件执行的语句
+#     elif 条件语句：
+#         满足条件执行的语句
+#     else：
+#         满足条件执行的语句
+
+# num = int(input("输入所猜数字："))
+# if num > 66:
+#     print("猜大了")
+# elif num < 66 :
+#     print("猜小了")
+# else:
+#     print("猜对了")
+#
+# achivement = int(input("请输入成绩："))
+# if achivement > 0:
+#     print('E')
+# elif achivement >39:
+#     print('D')
+# elif achivement >59:
+#     print('C')
+# elif achivement > 79:
+#     print('B')
+# elif 89 < achivement <= 100:
+#     print('A')
+#
+#
+# while 1:
+#     print(
+#     '''
+#     菜单：
+#         1：固网业务
+#         2：查话费
+#         3：话费充值
+#         *：退出
+#     ''')
+#     num = input("请选择：")
+#     if num == '1':
+#                 print('''
+#                 1.xxxxx
+#                 2.yyyyyyy
+#             ''')
+#     elif num == '2':
+#                 print('您的话费余额为：999999999999')
+#     elif num == '3':
+#                 input("请输入充值金额：")
+#                 print("充值成功")
+#     elif num == '*':
+#         print('结束')
+#         break
+# 第三章Python条件语句和运算符
+# 1.猜数字，设定一个理想数字比如：66，让用户输入数字，如果比66大，则显示猜测的结果大了；如果比66小，则显示猜测的结果小了;只有等于66，显示猜测结果正确，然后退出循环。
+# while 1:
+#     num = int(input("输入所猜数字："))
+#     if num > 66:
+#         print("猜大了")
+#     elif num < 66 :
+#         print("猜小了")
+#     else:
+#         print("猜对了")
+#         break
+# # 2.在上一题的基础，设置：给用户三次猜测机会，如果三次之内猜测对了，则显示猜测正确，退出循环，如果三次之内没有猜测正确，则自动退出循环，并显示‘大笨蛋’。
+# count = 0
+# while count < 3:
+#     num = int(input("输入所猜数字："))
+#     count += 1
+#     if num > 66:
+#         print("猜大了")
+#     elif num < 66:
+#         print("猜小了")
+#     else:
+#         print("猜对了")
+#         break
+# else:
+#     print("笨蛋")
+
+# 3.使用两种方法实现输出 1 2 3 4 5 6 8 9 10 。
+for i in range(1, 11):
+    print(i)
+i = 0
+while i < 10:
+    i += 1
+    print(i)
+
+# 4.求1-100的所有数的和
+print(sum(range(11)))
+# 5.输出 1-100 内的所有奇数
+for i in range(1,101,2):
+    print(i)
+# 6.输出 1-100 内的所有偶数
+for i in range(1,101,2):
+    print(i)
+# 7.求1-2+3-4+5 … 99的所有数的和
+
+# 8.⽤户登陆（三次输错机会）且每次输错误时显示剩余错误次数（提示：使⽤字符串格式化）
+
+# 9.猜年龄游戏
+# 要求：允许用户最多尝试3次，3次都没猜对的话，就直接退出，如果猜对了，打印恭喜信息并退出。
+#
+
+# 10.猜年龄游戏升级版
+# 要求：允许用户最多尝试3次，每尝试3次后，如果还没猜对，就问用户是否还想继续玩，如果回答Y，就继续让其猜3次，以此往复，如果回答N，就退出程序，如何猜对了，就直接退出。
+
+# 第四章Python的数据类型之字符串和整型
 name = "szk zeNb "
 # 移除 name 变量对应的值两边的空格,并输出处理结果
 print(name.strip())
@@ -79,13 +206,24 @@ print("是的") if str1.startswith("cc") else print("不是")
 print("是的") if str1.endswith("Nb") else print("不是")
 # 将 name 变量对应的值中的 所有的"l"替换为 "p",并输出结果
 name = "lpsdfafa"
-name.replace("l","p")
+print(name.replace("l","p"))
 # 写代码实现对用户输入的值判断，是否为十进制小数（isdecimal），如果是则转换为整型并输入，否则直接输出"请输入数字"
 
 # 对用户输入的数据使用"+"切割，判断输入的值是否都是数字？	提示：用户输入的格式必须是以+连接，如 5+9 、devops+999
-# 写代码实现一个整数加法计算器(两个数相加)
-# 需求：提示用户输入：5+9或5+9或5+9，计算出两个值的和（提示：先分割再转换为整型，再相加）
+num = input("请输入内容：（请用+连接）")
+for i in num.split("+"):
+    print(i.isalnum())
 
+# 写代码实现一个整数加法计算器(两个数相加)
+num = input("请输入内容：（数字+数字）")
+num_sum = 0
+for i in num.split("+"):
+    if i.isalnum():
+        i = int(i)
+        num_sum += i
+print(num_sum)
+
+# 需求：提示用户输入：5+9或5+9或5+9，计算出两个值的和（提示：先分割再转换为整型，再相加）
 
 
 a = 0
